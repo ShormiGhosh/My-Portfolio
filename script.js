3554075e1b0e7b495725a0d320ea8560a09b10fe
Shormi Ghosh <quanta5419@gmail.com>
2025-08-26 09:58:26 +0600

    designed a custom cursor

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
+// const gsap = window.gsap
+const ScrollTrigger = window.ScrollTrigger;
+
 //  -----------year script-----------
-
 document.getElementById("year").textContent = new Date().getFullYear();
-
-const gsap = window.gsap 
-const ScrollTrigger = window.ScrollTrigger 
+//custom cursor
+const cursor = document.querySelector(".cursor");
+const cursorInner = document.querySelector(".cursor-inner");
+if (window.innerWidth > 768) {
+  document.addEventListener("mousemove", (e) => {
+    gsap.to(cursor, {
+      left: e.clientX - 6,
+      top: e.clientY - 6,
+      duration: 0.1,
+    });
+    gsap.to(cursorInner, {
+      left: e.clientX - 14,
+      top: e.clientY - 14,
+      duration: 0.2,
+    });
+  });
+}
 // loading
 function initializeLoader() {
   const loader = document.querySelector(".loader");
   const txt = document.querySelector(".txt");
   const spinner = document.querySelector(".spinner");
-  gsap.to(txt,{
+  gsap.to(txt, {
     opacity: 1,
     duration: 0.6,
-    ease: "power2.out"
+    ease: "power2.out",
   });
-  gsap.to(spinner,{
+  gsap.to(spinner, {
     width: "100%",
     duration: 2,
     ease: "power2.inOut",
@@ -22,12 +38,13 @@ function initializeLoader() {
     onComplete: () => {
       gsap.to(loader, {
         opacity: 0,
-        onComplete: () =>    {
+        duration: 0.6,
+        onComplete: () => {
           loader.style.display = "none";
           initializeAnime();
-        }
+        },
       });
-    }
+    },
   });
 }
 window.addEventListener("load", initializeLoader);
@@ -35,6 +52,19 @@ function initializeAnime() {
   gsap.to("nav", {
     y: 0,
     duration: 1,
-    ease: "power3.out" 
+    ease: "power3.out",
   });
 }
+//themeToggle
+const themeToggle = document.getElementById("themeToggle");
+const body = document.body;
+
+const curr = localStorage.getItem("theme") || "dark";
+body.setAttribute("data-theme", curr);
+
+themeToggle.addEventListener("click", () => {
+  const current = body.getAttribute("data-theme");
+  const newTheme = current === "light" ? "dark" : "light";
+  body.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+});
